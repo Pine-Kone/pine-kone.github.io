e9d88a4822061d99dd584ca4935408d8dc58830e
Hans M. Haroldsen <hans.m.haroldsen@hotmail.com>
2026-08-14 14:26:37 -0600

    Add Turtwig, Grotle, and Torterra card data

---
+++ hobby/pokemon/chase-list/turtwig-grotle-torterra/turtwig-grotle-torterra.js
@@ -0,0 +1,990 @@
+"use strict";
+
+/*
+ * Turtwig, Grotle & Torterra Collection Data
+ *
+ * obtained:
+ *   true  = card/variant is in my collection
+ *   false = card/variant is still on my chase list
+ *
+ * obtainedDate:
+ *   YYYY-MM-DD
+ *
+ * obtainedLocation:
+ *   Where I obtained the card
+ *
+ * features:
+ *   Structured characteristics of the physical card
+ *
+ * notes:
+ *   Free-form notes about the specific card
+ *
+ * variants:
+ *   Used when a card has multiple collectible variations.
+ *
+ *   Each variation can have its own:
+ *     - type
+ *     - image
+ *     - obtained status
+ *     - date
+ *     - location
+ *     - features
+ *     - notes
+ */
+
+
+const turtwigGrotleTorterraCards = [
+
+  /* ================================
+     TURTWIG
+     ================================ */
+
+  {
+    name: "Turtwig",
+    set: "Diamond and Pearl",
+    number: "103 / 130",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Turtwig",
+    set: "POP Series 6",
+    number: "17 / 17",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Turtwig",
+    set: "Majestic Dawn",
+    number: "77 / 100",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Turtwig",
+    set: "Majestic Dawn",
+    number: "78 / 100",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Turtwig",
+    set: "POP Series 8",
+    number: "17 / 17",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Turtwig",
+    set: "Platinum",
+    number: "101 / 127",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Turtwig",
+    set: "POP Series 9",
+    number: "17 / 17",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Turtwig",
+    set: "Rising Rivals",
+    number: "85 / 111",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Turtwig",
+    set: "Supreme Victors",
+    number: "131 / 147",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Turtwig",
+    set: "DP Promo",
+    number: "DP1",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Turtwig",
+    set: "Unleashed",
+    number: "67 / 95",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Turtwig",
+    set: "Plasma Storm",
+    number: "1 / 135",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Turtwig",
+    set: "Ultra Prism",
+    number: "6 / 156",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Turtwig",
+    set: "Ultra Prism",
+    number: "7 / 156",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Turtwig",
+    set: "Brilliant Stars",
+    number: "6 / 172",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Turtwig",
+    set: "Crown Zenith",
+    number: "GG31 / GG70",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Turtwig",
+    set: "Temporal Forces",
+    number: "10 / 162",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Turtwig",
+    set: "Mega Promos",
+    number: "40",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+
+  /* ================================
+     GROTLE
+     ================================ */
+
+  {
+    name: "Grotle",
+    set: "Diamond and Pearl",
+    number: "49 / 130",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Grotle",
+    set: "Majestic Dawn",
+    number: "37 / 100",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Grotle",
+    set: "DP-P Promos",
+    number: "34 / DP-P",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Grotle",
+    set: "Platinum",
+    number: "49 / 127",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Grotle",
+    set: "Unleashed",
+    number: "31 / 95",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Grotle",
+    set: "Plasma Storm",
+    number: "2 / 135",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Grotle",
+    set: "Ultra Prism",
+    number: "8 / 156",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Grotle",
+    set: "Brilliant Stars",
+    number: "7 / 172",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Grotle",
+    set: "Temporal Forces",
+    number: "11 / 162",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Grotle",
+    set: "Temporal Forces",
+    number: "164 / 162",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+
+  /* ================================
+     TORTERRA
+     ================================ */
+
+  {
+    name: "Torterra",
+    set: "Diamond and Pearl",
+    number: "17 / 130",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Torterra Lv. X",
+    set: "Diamond and Pearl",
+    number: "122 / 130",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Torterra",
+    set: "Majestic Dawn",
+    number: "30 / 100",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Torterra",
+    set: "Stormfront",
+    number: "11 / 100",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Torterra",
+    set: "Platinum",
+    number: "39 / 127",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Torterra Lv. X",
+    set: "DP Promo",
+    number: "DP9",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Torterra",
+    set: "Unleashed",
+    number: "10 / 95",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Torterra",
+    set: "Plasma Storm",
+    number: "3 / 135",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Torterra",
+    set: "Ultra Prism",
+    number: "9 / 156",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Torterra",
+    set: "Forbidden Light",
+    number: "58 / 131",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Torterra",
+    set: "Brilliant Stars",
+    number: "8 / 172",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Torterra ex",
+    set: "Temporal Forces",
+    number: "12 / 162",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Torterra ex",
+    set: "Temporal Forces",
+    number: "185 / 162",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  }
+
+];
+
+
+/* ====================================
+   Render Collection
+   ==================================== */
+
+function renderCollection() {
+
+  const grid = document.getElementById("card-grid");
+
+  let totalCards = 0;
+  let obtainedCards = 0;
+
+  grid.innerHTML = "";
+
+
+  turtwigGrotleTorterraCards.forEach((card) => {
+
+    /*
+     * Each card can contain multiple variants.
+     * Count each variant individually.
+     */
+    const variants = card.variants || [];
+
+    totalCards += variants.length;
+
+    obtainedCards += variants.filter(
+      variant => variant.obtained
+    ).length;
+
+
+    const cardElement = document.createElement("article");
+
+    const cardObtained =
+      variants.some(variant => variant.obtained);
+
+
+    cardElement.className =
+      `tcg-card ${cardObtained ? "obtained" : "not-obtained"}`;
+
+
+    const variantsHTML = variants.map((variant) => {
+
+      const statusText =
+        variant.obtained
+          ? "✓ Obtained"
+          : "○ Still Chasing";
+
+
+      const statusClass =
+        variant.obtained
+          ? "status-obtained"
+          : "status-not-obtained";
+
+
+      const imageHTML = variant.image
+        ? `
+          <img
+            class="variant-image"
+            src="${variant.image}"
+            alt="${card.name} — ${card.set} ${card.number} — ${variant.type}"
+            loading="lazy"
+          >
+        `
+        : `
+          <div class="card-image-placeholder">
+            Image not added yet.
+          </div>
+        `;
+
+
+      const acquisitionHTML =
+        variant.obtained
+          ? `
+            <div class="acquisition-info">
+
+              <p>
+                <span class="acquisition-label">
+                  Date:
+                </span>
+
+                ${variant.obtainedDate || "Not recorded"}
+              </p>
+
+              <p>
+                <span class="acquisition-label">
+                  Location:
+                </span>
+
+                ${variant.obtainedLocation || "Not recorded"}
+              </p>
+
+            </div>
+          `
+          : "";
+
+
+      const featuresHTML =
+        variant.features &&
+        variant.features.length > 0
+          ? `
+            <div class="card-features">
+
+              <p class="features-label">
+                Features:
+              </p>
+
+              <ul class="features-list">
+
+                ${variant.features
+                  .map(feature => `<li>${feature}</li>`)
+                  .join("")}
+
+              </ul>
+
+            </div>
+          `
+          : "";
+
+
+      const notesHTML =
+        variant.notes &&
+        variant.notes.length > 0
+          ? `
+            <div class="card-notes">
+
+              <p class="notes-label">
+                Notes:
+              </p>
+
+              <ul class="notes-list">
+
+                ${variant.notes
+                  .map(note => `<li>${note}</li>`)
+                  .join("")}
+
+              </ul>
+
+            </div>
+          `
+          : "";
+
+
+      return `
+        <div class="variant">
+
+          <div class="variant-name">
+            ${variant.type}
+          </div>
+
+          ${imageHTML}
+
+          <div class="variant-status">
+            <span class="ownership-status ${statusClass}">
+              ${statusText}
+            </span>
+          </div>
+
+          ${acquisitionHTML}
+
+          ${featuresHTML}
+
+          ${notesHTML}
+
+        </div>
+      `;
+
+    }).join("");
+
+
+    cardElement.innerHTML = `
+
+      <div class="card-info">
+
+        <h3 class="card-name">
+          ${card.name}
+        </h3>
+
+        <p class="card-set">
+          ${card.set}
+        </p>
+
+        <p class="card-number">
+          ${card.number}
+        </p>
+
+        <div class="card-variants">
+
+          <p class="variants-label">
+            Variations:
+          </p>
+
+          ${variantsHTML}
+
+        </div>
+
+      </div>
+
+    `;
+
+
+    grid.appendChild(cardElement);
+
+  });
+
+
+  const remainingCards =
+    totalCards - obtainedCards;
+
+
+  const completion =
+    totalCards === 0
+      ? 0
+      : Math.round(
+          (obtainedCards / totalCards) * 100
+        );
+
+
+  document.getElementById("total-cards").textContent =
+    totalCards;
+
+  document.getElementById("obtained-cards").textContent =
+    obtainedCards;
+
+  document.getElementById("remaining-cards").textContent =
+    remainingCards;
+
+  document.getElementById("completion-percent").textContent =
+    `${completion}%`;
+
+}
+
+
+/* ====================================
+   Start Page
+   ==================================== */
+
+document.addEventListener(
+  "DOMContentLoaded",
+  renderCollection
+);
