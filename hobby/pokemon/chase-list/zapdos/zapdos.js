5006d7c66880e3d155cf59dd43a8d9bab924faab
Hans M. Haroldsen <hans.m.haroldsen@hotmail.com>
2026-08-22 00:36:38 -0600

    Add Zapdos collection data and page rendering
    This file contains the collection data for Zapdos cards, including their sets, numbers, and obtained status. It initializes the chase list page with the Zapdos card data.

---
+++ hobby/pokemon/chase-list/zapdos/zapdos.js
@@ -0,0 +1,918 @@
+"use strict";
+
+/*
+ * Zapdos Collection Data
+ * See /scripts/hobby/pokemon/chase-list/chase-list-common.js for the
+ * card/variant shape and field descriptions.
+ *
+ * Pulled from Serebii's card list for Zapdos; ownership defaults to
+ * not-obtained across the board. Update `obtained`, `obtainedDate`,
+ * `obtainedLocation`, and `image` as cards are acquired or
+ * photographed.
+ */
+
+const zapdosCards = [
+
+  {
+    name: "Zapdos",
+    set: "Base",
+    number: "16 / 102",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "Fossil",
+    number: "15 / 62",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "Fossil",
+    number: "30 / 62",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/zapdos/zapdos-30-62.jpeg",
+        obtained: true,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rocket's Zapdos",
+    set: "Gym Challenge",
+    number: "15 / 132",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/zapdos/rockets-zapdos-15-132-25th-anniversary.jpeg",
+        obtained: true,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "Wizards Promos",
+    number: "23",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "Vending Machine Set 2",
+    number: "",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "Aquapolis",
+    number: "44 / 147",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "P Promos",
+    number: "24 / 47",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos ex",
+    set: "EX FireRed and Leaf Green",
+    number: "116 / 112",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rocket's Zapdos ex",
+    set: "EX Team Rocket Returns",
+    number: "106 / 109",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos ex",
+    set: "Nintendo Promo",
+    number: "33",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "Majestic Dawn",
+    number: "14 / 100",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "Supreme Victors",
+    number: "48 / 147",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "Supreme Victors",
+    number: "150 / 147",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "Arceus",
+    number: "12 / 99",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "Pokemon Rumble",
+    number: "8 / 16",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "Next Destinies",
+    number: "41 / 99",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos EX",
+    set: "Plasma Storm",
+    number: "48 / 135",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "Legendary Treasures",
+    number: "46 / 113",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/zapdos/zapdos-46-113.jpeg",
+        obtained: true,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "Roaring Skies",
+    number: "23 / 108",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "Generations",
+    number: "29 / 83",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/zapdos/zapdos-29-83.jpeg",
+        obtained: true,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "Evolutions",
+    number: "42 / 108",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "Team Up",
+    number: "40 / 181",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "Hidden Fates",
+    number: "24 / 68",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Moltres & Zapdos & Articuno GX",
+    set: "Hidden Fates",
+    number: "44 / 68",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Moltres & Zapdos & Articuno GX",
+    set: "Hidden Fates",
+    number: "66 / 68",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Moltres & Zapdos & Articuno GX",
+    set: "Hidden Fates",
+    number: "69 / 68",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Moltres, Zapdos & Articuno GX",
+    set: "Tag All Stars",
+    number: "226 / 173",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "SM Promos",
+    number: "SM145",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "SM Promos",
+    number: "SM159",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Moltres, Zapdos & Articuno GX",
+    set: "SM Promos",
+    number: "SM210",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "Vivid Voltage",
+    number: "48 / 180",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      },
+      {
+        type: "Reverse Holo",
+        image: "/images/pokemon/chase-list/zapdos/zapdos-48-185-reverse-holo.jpeg",
+        obtained: true,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: ["Reverse Holo"],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Galarian Zapdos V",
+    set: "Chilling Reign",
+    number: "80 / 198",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Galarian Zapdos V",
+    set: "Chilling Reign",
+    number: "173 / 198",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Galarian Zapdos V",
+    set: "Chilling Reign",
+    number: "174 / 198",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Galarian Zapdos",
+    set: "Evolving Skies",
+    number: "82 / 225",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rocket's Zapdos",
+    set: "Celebrations",
+    number: "15 / 132",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Galarian Zapdos V",
+    set: "Brilliant Stars",
+    number: "182 / 172",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Galarian Zapdos V",
+    set: "Astral Radiance",
+    number: "TG19 / TG30",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "Pokemon GO",
+    number: "29 / 78",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Galarian Zapdos",
+    set: "SWSH Promos",
+    number: "SWSH124",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Galarian Zapdos",
+    set: "SWSH Promos",
+    number: "SWSH283",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos ex",
+    set: "151",
+    number: "145 / 165",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/zapdos/zapdos-ex-145-165.jpeg",
+        obtained: true,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos ex",
+    set: "151",
+    number: "192 / 165",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos ex",
+    set: "151",
+    number: "202 / 165",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "Twilight Masquerade",
+    number: "65 / 167",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/zapdos/zapdos-65-167.jpeg",
+        obtained: true,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Team Rocket's Zapdos",
+    set: "Destined Rivals",
+    number: "70 / 182",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos ex",
+    set: "SV Promos",
+    number: "49",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "SV Promos",
+    number: "157",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "30th Celebration",
+    number: "55 / 128",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Zapdos",
+    set: "Mega Promos",
+    number: "98",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  }
+
+];
+
+
+/* ====================================
+   Start Page
+   ==================================== */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  renderChaseListPage({
+
+    groups: [
+      { gridId: "card-grid", cards: zapdosCards }
+    ],
+
+    statsIds: {
+      total: "total-cards",
+      obtained: "obtained-cards",
+      remaining: "remaining-cards",
+      percent: "completion-percent"
+    },
+
+    filterId: "collection-filter"
+
+  });
+
+});
