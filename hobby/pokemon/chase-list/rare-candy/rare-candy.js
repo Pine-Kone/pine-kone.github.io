c7e0bd9a27c12366f78f6e67f5ca31178832d0e4
Hans M. Haroldsen <hans.m.haroldsen@hotmail.com>
2026-08-22 00:45:10 -0600

    Add Rare Candy collection data
    This file contains data for the Rare Candy collection, including card details such as name, set, number, and obtained status.

---
+++ hobby/pokemon/chase-list/rare-candy/rare-candy.js
@@ -0,0 +1,569 @@
+"use strict";
+
+/*
+ * Rare Candy Collection Data
+ * See /scripts/hobby/pokemon/chase-list/chase-list-common.js for the
+ * card/variant shape and field descriptions.
+ *
+ * Pulled from Serebii's Rare Candy trainer-card list; ownership
+ * defaults to not-obtained across the board. Update `obtained`,
+ * `obtainedDate`, `obtainedLocation`, and `image` as cards are
+ * acquired or photographed.
+ */
+
+const rareCandyCards = [
+
+  {
+    name: "Rare Candy",
+    set: "EX Sandstorm",
+    number: "88 / 100",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-88-100.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "EX Emerald",
+    number: "83 / 106",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-83-106.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "EX Holon Phantoms",
+    number: "90 / 110",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-90-110.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "POP Series 5",
+    number: "7 / 17",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-7-17.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "Great Encounters",
+    number: "102 / 106",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-102-106.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "POP Series 8",
+    number: "10 / 17",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-10-17.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "Unleashed",
+    number: "82 / 95",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-82-95.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "Dark Explorers",
+    number: "100 / 108",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-100-108.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "Plasma Blast",
+    number: "85 / 101",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-85-101.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "Plasma Blast",
+    number: "105 / 101",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-105-101.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "Battle Strength Deck Blastoise Kyurem EX",
+    number: "10 / 18",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-10-18.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "BW Promo",
+    number: "138 / BW-P",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-138-bw-p.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "Primal Clash",
+    number: "135 / 160",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-135-160.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "XY Promo",
+    number: "136 / XY-P",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-136-xy-p.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "Sun Moon",
+    number: "129 / 149",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-129-149.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "Guardians Rising",
+    number: "165 / 145",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-165-145.jpeg",
+        obtained: true,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "Celestial Storm",
+    number: "142 / 168",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-142-168.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "SM Promo",
+    number: "25 / SM-P",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-25-sm-p.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "Sword Shield",
+    number: "180 / 202",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-180-202.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "Pokemon GO",
+    number: "69 / 78",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-69-78.jpeg",
+        obtained: true,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "Crown Zenith",
+    number: "141 / 159",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-141-159.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "S Promo",
+    number: "52 / 400",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-52-400.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "Scarlet Violet",
+    number: "191 / 198",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-191-198.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "Scarlet Violet",
+    number: "256 / 198",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-256-198.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "Paldean Fates",
+    number: "89 / 91",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-89-91.jpeg",
+        obtained: true,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "SV Promo",
+    number: "16 / 200",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-16-200.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "SV Promo",
+    number: "45 / 200",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-45-200.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "SV Promo",
+    number: "159 / 200",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-159-200.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "SV Promo",
+    number: "246 / 200",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-246-200.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "Mega Evolution",
+    number: "125 / 132",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-125-132.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Rare Candy",
+    set: "Mega Evolution",
+    number: "175 / 132",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/rare-candy/rare-candy-175-132.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  }
+
+];
+
+
+/* ====================================
+   Start Page
+   ==================================== */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  renderChaseListPage({
+
+    groups: [
+      { gridId: "card-grid", cards: rareCandyCards }
+    ],
+
+    statsIds: {
+      total: "total-cards",
+      obtained: "obtained-cards",
+      remaining: "remaining-cards",
+      percent: "completion-percent"
+    },
+
+    filterId: "collection-filter"
+
+  });
+
+});
