1269345ea0257de46a2c15e1bbac803982209e34
Hans M. Haroldsen <hans.m.haroldsen@hotmail.com>
2026-08-22 00:30:40 -0600

    Add Pineco and Forretress collection data
    This file contains the collection data for Pineco and Forretress cards, including their sets, numbers, variants, and obtained status.

---
+++ hobby/pokemon/chase-list/pineco-forretress/pineco-forretress.js
@@ -0,0 +1,849 @@
+"use strict";
+
+/*
+ * Pineco & Forretress Collection Data
+ * See /scripts/hobby/pokemon/chase-list/chase-list-common.js for the
+ * card/variant shape and field descriptions.
+ *
+ * Pulled from Serebii's card lists for Pineco and Forretress; ownership
+ * defaults to not-obtained across the board. Update `obtained`,
+ * `obtainedDate`, `obtainedLocation`, and `image` as cards are
+ * acquired or photographed.
+ */
+
+const pinecoCards = [
+
+  {
+    name: "Pineco",
+    set: "Neo Discovery",
+    number: "61 / 75",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Pineco",
+    set: "Neo Destiny",
+    number: "77 / 105",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Pineco",
+    set: "Skyridge",
+    number: "85 / 144",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Pineco",
+    set: "Skyridge",
+    number: "86 / 144",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Pineco",
+    set: "EX Dragon",
+    number: "71 / 97",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Pineco",
+    set: "EX Unseen Forces",
+    number: "66 / 115",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Pineco",
+    set: "Legends Awakened",
+    number: "113 / 146",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Pineco",
+    set: "Undaunted",
+    number: "62 / 90",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Pineco",
+    set: "Undaunted",
+    number: "63 / 90",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Pineco",
+    set: "Call of Legends",
+    number: "68 / 95",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Pineco",
+    set: "Flashfire",
+    number: "4 / 106",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Pineco",
+    set: "Lost Thunder",
+    number: "15 / 214",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Pineco",
+    set: "Vivid Voltage",
+    number: "8 / 180",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Pineco",
+    set: "Scarlet Violet",
+    number: "1 / 198",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/pineco-forretress/pineco-1-198.jpeg",
+        obtained: true,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Pineco",
+    set: "Paldea Evolved",
+    number: "4 / 198",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Pineco",
+    set: "Paldean Fates",
+    number: "1 / 91",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/pineco-forretress/pineco-1-91.jpeg",
+        obtained: true,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      },
+      {
+        type: "Reverse Holo",
+        image: "/images/pokemon/chase-list/pineco-forretress/pineco-1-91-reverse-holo.jpeg",
+        obtained: true,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: ["Reverse Holo"],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Pineco",
+    set: "Paldean Fates",
+    number: "99 / 91",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Pineco",
+    set: "Temporal Forces",
+    number: "2 / 162",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/pineco-forretress/pineco-2-162.jpeg",
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      },
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/pineco-forretress/pineco-2-162-reverse-holo.jpeg",
+        obtained: true,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Pineco",
+    set: "Destined Rivals",
+    number: "4 / 182",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/pineco-forretress/pineco-4-182.jpeg",
+        obtained: true,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Pineco",
+    set: "SV Promos",
+    number: "61",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  }
+
+];
+
+
+const forretressCards = [
+
+  {
+    name: "Forretress",
+    set: "Neo Discovery",
+    number: "2 / 75",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Forretress",
+    set: "Neo Discovery",
+    number: "21 / 75",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Dark Forretress",
+    set: "Neo Destiny",
+    number: "35 / 105",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Koga's Forretress",
+    set: "VS",
+    number: "80 / 141",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Forretress",
+    set: "Skyridge",
+    number: "9 / 144",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Forretress",
+    set: "Skyridge",
+    number: "56 / 144",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Forretress",
+    set: "EX Dragon",
+    number: "28 / 97",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Forretress",
+    set: "EX Unseen Forces",
+    number: "6 / 115",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Forretress",
+    set: "POP Series 3",
+    number: "9 / 17",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Brock's Forretress",
+    set: "8th Movie Halfdeck",
+    number: "11 / 20",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Forretress",
+    set: "Legends Awakened",
+    number: "28 / 146",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Forretress",
+    set: "Rising Rivals",
+    number: "61 / 111",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Forretress",
+    set: "Undaunted",
+    number: "3 / 90",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Forretress",
+    set: "Undaunted",
+    number: "13 / 90",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Forretress",
+    set: "Call of Legends",
+    number: "5 / 95",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Forretress",
+    set: "Flashfire",
+    number: "60 / 106",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Forretress",
+    set: "Lost Thunder",
+    number: "124 / 214",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Forretress",
+    set: "Vivid Voltage",
+    number: "114 / 185",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      },
+      {
+        type: "Reverse Holo",
+        image: "/images/pokemon/chase-list/pineco-forretress/forretress-114-185-reverse-holo.jpeg",
+        obtained: true,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: ["Reverse Holo"],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Forretress",
+    set: "Scarlet Violet",
+    number: "139 / 198",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/pineco-forretress/forretress-139-198.jpeg",
+        obtained: true,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Forretress ex",
+    set: "Paldea Evolved",
+    number: "5 / 198",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Forretress ex",
+    set: "Paldea Evolved",
+    number: "230 / 198",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Forretress ex",
+    set: "Paldean Fates",
+    number: "2 / 91",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Forretress ex",
+    set: "Paldean Fates",
+    number: "212 / 91",
+    variants: [
+      {
+        type: "Standard",
+        image: null,
+        obtained: false,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Forretress",
+    set: "Temporal Forces",
+    number: "110 / 162",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/pineco-forretress/forretress-110-162.jpeg",
+        obtained: true,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      },
+      {
+        type: "Reverse Holo",
+        image: "/images/pokemon/chase-list/pineco-forretress/forretress-110-162-reverse-holo.jpeg",
+        obtained: true,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: ["Reverse Holo"],
+        notes: []
+      }
+    ]
+  },
+
+  {
+    name: "Forretress",
+    set: "Destined Rivals",
+    number: "140 / 182",
+    variants: [
+      {
+        type: "Standard",
+        image: "/images/pokemon/chase-list/pineco-forretress/forretress-140-182.jpeg",
+        obtained: true,
+        obtainedDate: null,
+        obtainedLocation: null,
+        features: [],
+        notes: []
+      }
+    ]
+  }
+
+];
+
+
+/* ====================================
+   Start Page
+   ==================================== */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  renderChaseListPage({
+
+    groups: [
+      { gridId: "pineco-grid", cards: pinecoCards },
+      { gridId: "forretress-grid", cards: forretressCards }
+    ],
+
+    statsIds: {
+      total: "total-cards",
+      obtained: "obtained-cards",
+      remaining: "remaining-cards",
+      percent: "completion-percent"
+    },
+
+    filterId: "collection-filter"
+
+  });
+
+});
